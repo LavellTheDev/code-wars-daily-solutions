8a08fdbddaad6ce2cc52fffbd800179901a7e46f
RizelBobb <rizel.bobbsemple137@gmail.com>
2018-03-20 00:46:12 -0400

    added new codewars

---
+++ covert-number-reverse/server.js
@@ -0,0 +1,19 @@
+/*Convert number to reversed array of digits
+
+Given a random number:
+
+    C#: long;
+    C++: unsigned long;
+
+You have to return the digits of this number within an array in reverse order.*/
+
+function digitize(n) {
+  var initialArray = (""+n).split('');
+  var reversedArray = [];
+  for (var i = initialArray.length - 1; i >= 0; i--) {
+    reversedArray[i] = parseInt(initialArray.shift(),10);
+  }
+  return reversedArray;
+}
+
+console.log(digitize(35231));
